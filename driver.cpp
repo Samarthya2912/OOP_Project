be46250e1c9466111bcd7fc9ad52743e62013834
Samarthya <samarthya55@gmail.com>
2021-11-11 10:43:25 +0530

    binaryTree

--- driver.cpp
+++ driver.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
+#include <time.h>
 #include "./myLibrary/array.cpp"
+#include "./myLibrary/binaryTree.cpp"
 using namespace std;
 
 int increment(int i, int index) {
@@ -16,6 +18,16 @@ int doubler(int i) {
 }
 
 int main() {
-    Array<int> t(10, doubler);
-    cout << t;
+    binaryTree<int> a;
+    a.insert(44);
+    a.insert(7);
+    a.insert(8);
+    a.insert(12);
+    a.insert(1);
+    a.insert(23);
+    a.insert(19);
+    a.insert(10);
+    srand(10);
+    a.inorder();
+    a.printTree();
 }
